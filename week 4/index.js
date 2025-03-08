8207a6bbf69e26ff70d618fc4baa9aed78a2d785
Festus O Ayobami <37890891+Psalmist2020@users.noreply.github.com>
2025-03-08 23:40:33 +0100

    Add files via upload

---
+++ week 4/index.js
@@ -0,0 +1,26 @@
+//calculating a value
+function add(number) {
+    return number + number
+}
+
+console.log(2 + 2)
+
+
+//greeting and date log
+function greet(Date) {
+    console.log ('Hello World' + ' ' + Date);
+}
+
+greet('1st March, 2025');
+
+
+//sequential number list
+let sequence = [1,2,3,4,5,6,7,8,9,10];
+console.log (sequence);
+
+//largest number
+function search() {
+    console.log (10);
+}
+
+search();
